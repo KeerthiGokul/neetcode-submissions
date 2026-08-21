b43d70b497ea90e7a2d3743bc1323a8b34af0eba
KeerthiGokul <81665417+KeerthiGokul@users.noreply.github.com>
2026-08-21 22:16:38 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = new Map();
+        for(let f of nums){
+            map.set(f,(map.get(f)||0)+1);
+        }
+        let result=[];
+        const temp = [...map];
+        temp.sort((a,b)=>b[1]-a[1]);
+        console.log(temp);
+        for(let i=0;i<k;i++){
+            result.push(temp[i][0]);
+        }        
+        return result;
+    }
+}
