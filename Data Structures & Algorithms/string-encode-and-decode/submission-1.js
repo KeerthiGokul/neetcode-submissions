c50ec8c6da9b1b50decee8bc8fb423f3f6a1bd00
KeerthiGokul <81665417+KeerthiGokul@users.noreply.github.com>
2026-08-24 19:41:18 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result="";
+        for(let k of strs){
+        result+=k.length+"#"+k;   
+        }    
+        return result;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let result=[];
+        let i=0;
+        while(i<str.length){
+            let j=i;
+            while(str[j]!=="#"){
+                j++;
+            }
+            let length = Number(str.slice(i,j));
+            console.log(length);
+            let word=str.slice(j+1,j+1+length);
+            result.push(word);
+
+            i=j+1+length;
+        }
+        return result;
+    }
+}
